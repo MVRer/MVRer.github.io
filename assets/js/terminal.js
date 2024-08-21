e97000937a211d1e1a389e90086405d536d9b3b6
Mariano <a01029708@tec.mx>
2024-08-21 13:44:44 -0600

    Animaciones

--- assets/js/terminal.js
+++ assets/js/terminal.js
@@ -3,36 +3,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const content = container.innerHTML;
     container.innerHTML = '> Initializing...\n\n';
 
-    function typeWriter(text, i, fnCallback) {
-        if (i < text.length) {
-            container.innerHTML = container.innerHTML.slice(0, -1) + text.charAt(i) + "_";
-            setTimeout(function() {
-                typeWriter(text, i + 1, fnCallback)
-            }, 5);
-        } else if (typeof fnCallback == 'function') {
-            setTimeout(fnCallback, 700);
+    function typeWriter(text, fnCallback) {
+        const typingSpeed = Math.max(1, Math.floor(2000 / text.length)); // Ajusta la velocidad basada en la longitud del texto
+        let i = 0;
+        function type() {
+            if (i < text.length) {
+                container.innerHTML += text.charAt(i);
+                i++;
+                setTimeout(type, typingSpeed);
+            } else if (typeof fnCallback == 'function') {
+                fnCallback();
+            }
         }
+        type();
     }
 
-    function startTextAnimation(i) {
-        const contentArray = content.split('\n');
-        if (i < contentArray.length) {
-            typeWriter(contentArray[i] + '\n', 0, function() {
-                startTextAnimation(i + 1);
-            });
-        } else {
-            // Restore the original content to ensure all event listeners are intact
-            container.innerHTML = content;
-            // Add blinking cursor
-            const cursor = document.createElement('div');
-            cursor.className = 'blink';
-            cursor.textContent = '> _';
-            container.appendChild(cursor);
-        }
+    function startTextAnimation() {
+        // Anima solo el título y un breve mensaje de bienvenida
+        const title = document.querySelector('h1').textContent;
+        const welcomeMessage = "Bienvenido al terminal. Cargando contenido...";
+        
+        typeWriter(title + '\n\n' + welcomeMessage, function() {
+            // Restaura el contenido original después de la animación
+            setTimeout(() => {
+                container.innerHTML = content;
+                // Añade el cursor parpadeante
+                const cursor = document.createElement('div');
+                cursor.className = 'blink';
+                cursor.textContent = '> _';
+                container.appendChild(cursor);
+            }, 500);
+        });
     }
 
-    setTimeout(function() {
-        container.innerHTML = '';
-        startTextAnimation(0);
-    }, 1000);
+    setTimeout(startTextAnimation, 100);
 });
